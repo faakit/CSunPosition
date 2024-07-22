a3a826dd8ae36fc0b9174e9f2bcf0a765fe3b727
otavioassis-git <otavioassis2013@gmail.com>
2024-07-22 11:10:38 -0300

    adding a test to read from serial using node

---
+++ sunPage/SerialReader.ts
@@ -0,0 +1,20 @@
+const { SerialPort } = require("serialport");
+import { ReadlineParser as Readline } from "@serialport/parser-readline";
+
+// mudem isso aqui pra porta e o baudRate que vcs estão usando aí
+const port = new SerialPort({
+  path: "/dev/ttyACM0",
+  baudRate: 9600,
+});
+
+port.write("SerialReader connected", function (err) {
+  if (err) {
+    return console.log("Error on write: ", err.message);
+  }
+  console.log("message written");
+});
+
+// Read data that is available but keep the stream in "paused mode"
+port.on('data', function (data) {
+  console.log('Data:', data)
+})
