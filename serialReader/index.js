cc680aa910ef1a976d462630a8fb61429e5f9a25
otavioassis-git <otavioassis2013@gmail.com>
2024-07-24 15:23:52 -0300

    fix: serial port connection

--- serialReader/index.js
+++ serialReader/index.js
@@ -22,17 +22,22 @@ const { SerialPort } = require("serialport");
 var arduinoPort = new SerialPort({
   path: "/dev/ttyACM0",
   baudRate: 115200,
+  autoOpen: false,
 });
 
-// mudem isso aqui pra porta e o baudRate que vcs estão usando aí
-arduinoPort.write("SerialReader connected", function (err) {
+arduinoPort.open(function (err) {
   if (err) {
-    return console.log("Error on write: ", err.message);
+    return console.log('Error opening port: ', err.message)
   }
-  console.log("message written");
-});
 
-// Read data that is available but keep the stream in "paused mode"
+  arduinoPort.write("SerialReader connected", function (err) {
+    if (err) {
+      return console.log("Error on write: ", err.message);
+    }
+    console.log("message written");
+  });
+})
+
 arduinoPort.on("data", function (data) {
   console.log("Data:", data);
   io.emit('serialdata', { data: data });
