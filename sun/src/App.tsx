dd5185b22ec64e66fe4022d816dc42ac2857cc33
Otávio Fava Souza de Assis <72041544+otavioassis-git@users.noreply.github.com>
2024-07-26 09:55:59 -0300

    fix: socket subscription

--- sun/src/App.tsx
+++ sun/src/App.tsx
@@ -4,21 +4,34 @@ import oceanImage from "./assets/ocean.png";
 import { useEffect, useState } from "react";
 import { Slider } from "@mui/material";
 import styled from "@emotion/styled";
-import io from "socket.io-client";
+import io, { Socket } from "socket.io-client";
+
+function parseHour(hourRaw: number): string {
+  const minute = Math.round((hourRaw % 1) * 60);
+  const hour = Math.floor(hourRaw)
+
+  return `${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute}`
+}
 
 export default function App() {
   const [position, setPosition] = useState(sunPosition(0));
   const [time, setTime] = useState(0);
-  const socket = io("http://localhost:8888");
+  const [socket, setSocket] = useState<Socket>();
 
   useEffect(() => {
-    socket.on("serial", ({ data }) => {
-      console.log("Saida serial arduino: ", data);
-      const parsedNumber = Number(data.replace("\r", ""));
-      setTime(parsedNumber);
-      setPosition(sunPosition(parsedNumber));
-    });
+    setSocket(io("http://localhost:8888"));
   }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("serial", ({ data }) => {
+        console.log("Saida serial arduino: ", data);
+        const parsedNumber = Number(data.replace("\r", ""));
+        setTime(parsedNumber);
+        setPosition(sunPosition(parsedNumber));
+      });
+    }
+  }, [socket])
 
   function sunPosition(time: number): { x: number; y: number } {
     const maxWidth = window.innerWidth + 500;
@@ -26,7 +39,7 @@ export default function App() {
     const maxHeight = window.innerHeight;
 
     return {
-      x: width - 450,
+      x: -width + 2000,
       y:
         (((2 * Math.sqrt(maxHeight)) / maxWidth) * width -
           Math.sqrt(maxHeight)) **
@@ -55,7 +68,7 @@ export default function App() {
 
   return (
     <Container>
-      <Hours>{time} horas</Hours>
+      <Hours>{parseHour(time)}</Hours>
       <Slider
         sx={{ zIndex: "3", width: "500px" }}
         min={0}
@@ -95,8 +108,10 @@ const Container = styled.div`
 const Hours = styled.p`
   position: absolute;
   top: 16px;
-  right: 16px;
+  right: 50%;
   color: #fff;
+  font-size: 24px;
+  font-weight: bold;
   z-index: 1;
 `;
 
@@ -106,18 +121,21 @@ const Stars = styled.img`
   left: 0;
   width: 100vw;
   z-index: 0;
+  transition: all 0.2s;
 `;
 
 const Sun = styled.img`
   position: absolute;
   height: 200px;
   z-index: 1;
+  transition: all 0.2s;
 `;
 
 const SunShadow = styled.div`
   position: absolute;
   box-shadow: 0px 0px 300px 200px #ffde8b;
   z-index: 0;
+  transition: all 0.2s;
 `;
 
 const Ocean = styled.img`
